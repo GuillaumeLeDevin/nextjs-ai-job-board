c29a493a1444a974e5d090e0702b1e52ca6f9c86
Guillaume Le Devin <guillaume.le.devin@gmail.com>
2025-11-23 00:11:34 +0000

    added the first function clerkCreateUser in route.ts

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,4 +1,5 @@
 import { inngest } from "@/services/inngest/client";
+import { clerCreateUser } from "@/services/inngest/functions/clerck";
 import { serve } from "inngest/next";
 
 
@@ -6,6 +7,6 @@ import { serve } from "inngest/next";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
+    clerCreateUser
   ],
 });
