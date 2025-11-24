01b7ffacf017d8f76fc448d8745da3e70fbac178
Guillaume Le Devin <guillaume.le.devin@gmail.com>
2025-11-24 17:16:30 +0000

    adjustment of the function with tests made

--- src/services/inngest/functions/clerck.ts
+++ src/services/inngest/functions/clerck.ts
@@ -2,8 +2,9 @@ import { env } from "@/data/env/server";
 import { inngest } from "../client";
 import { Webhook } from "svix";
 import { NonRetriableError } from "inngest";
-import { insertUser } from "@/app/features/users/db/users";
+import { deleteUser, insertUser, updateUser } from "@/app/features/users/db/users";
 import { insertUserNotificationSettings } from "@/app/features/users/db/userNotificationSettings";
+
 
 function verifyWebhook({ raw, headers }: {
     raw: string;
@@ -13,24 +14,21 @@ function verifyWebhook({ raw, headers }: {
 }
 
 export const clerkCreateUser = inngest.createFunction(
-    {
-        id: 'clerk/create-db-user',
-        name: "Clerk - Create DB User"
-    }, {
-        event: "clerk/user.created"
-    },
+    { id: 'clerk/create-db-user', name: "Clerk - Create DB User" },
+    { event: "clerk/user.created" },
+
     async ({ event, step }) => {
         await step.run("Verify Webhook", async () => {
             try {
                 verifyWebhook(event.data)
             } catch {
-                throw new NonRetriableError("Invalid webhook signature");
+                throw new NonRetriableError("Invalid webhook");
             }
         });
 
         const userId = await step.run("create-user", async () => {
             const userData = event.data.data
-            const email = userData.email_addresses?.find(e => e.id === userData.primary_email_address_id)?.email_address || null;
+            const email = userData.email_addresses?.find(email => email.id === userData.primary_email_address_id);
             if (!email) {
                 throw new NonRetriableError("No primary email address found");
             }
@@ -39,7 +37,7 @@ export const clerkCreateUser = inngest.createFunction(
                 id: userData.id,
                 name: `${userData.first_name} ${userData.last_name}`,
                 imageUrl: userData.image_url,
-                email: email,
+                email: email.email_address,
                 createdAt: new Date(userData.created_at),
                 updatedAt: new Date(userData.updated_at)
             })
@@ -53,33 +51,29 @@ export const clerkCreateUser = inngest.createFunction(
 });
 
 export const clerkUpdateUser = inngest.createFunction(
-    {
-        id: 'clerk/update-db-user',
-        name: "Clerk - Update DB User"
-    }, {
-        event: "clerk/user.updated"
-    },
+    { id: 'clerk/update-db-user', name: "Clerk - Update DB User" },
+    { event: "clerk/user.updated" },
+
     async ({ event, step }) => {
         await step.run("Verify Webhook", async () => {
             try {
                 verifyWebhook(event.data)
             } catch {
-                throw new NonRetriableError("Invalid webhook signature");
+                throw new NonRetriableError("Invalid webhook");
             }
         });
 
-        const userId = await step.run("update-user", async () => {
+        await step.run("update-user", async () => {
             const userData = event.data.data
-            const email = userData.email_addresses?.find(e => e.id === userData.primary_email_address_id)?.email_address || null;
+            const email = userData.email_addresses?.find(email => email.id === userData.primary_email_address_id);
             if (!email) {
                 throw new NonRetriableError("No primary email address found");
             }
 
-            await updateUser({
-                id: userData.id,
+            await updateUser(userData.id, {
                 name: `${userData.first_name} ${userData.last_name}`,
                 imageUrl: userData.image_url,
-                email: email,
+                email: email.email_address,
                 createdAt: new Date(userData.created_at),
                 updatedAt: new Date(userData.updated_at)
             })
@@ -89,37 +83,24 @@ export const clerkUpdateUser = inngest.createFunction(
 });
 
 export const clerkDeleteUser = inngest.createFunction(
-    {
-        id: 'clerk/delete-db-user',
-        name: "Clerk - Delete DB User"
-    }, {
-        event: "clerk/user.deleted"
-    },
+    { id: 'clerk/delete-db-user', name: "Clerk - Delete DB User" },
+    { event: "clerk/user.deleted" },
+
     async ({ event, step }) => {
         await step.run("Verify Webhook", async () => {
             try {
                 verifyWebhook(event.data)
             } catch {
-                throw new NonRetriableError("Invalid webhook signature");
+                throw new NonRetriableError("Invalid webhook");
             }
         });
 
-        const userId = await step.run("create-user", async () => {
-            const userData = event.data.data
-            const email = userData.email_addresses?.find(e => e.id === userData.primary_email_address_id)?.email_address || null;
-            if (!email) {
-                throw new NonRetriableError("No primary email address found");
+        await step.run("delete-user", async () => {
+            const {id} = event.data.data
+            if (id === null || id === undefined) {
+                throw new NonRetriableError("No user ID found");
             }
 
-            await deleteUser({
-                id: userData.id,
-                name: `${userData.first_name} ${userData.last_name}`,
-                imageUrl: userData.image_url,
-                email: email,
-                createdAt: new Date(userData.created_at),
-                updatedAt: new Date(userData.updated_at)
-            })
-
-            return userData.id;
+            await deleteUser(id)
         });
 });
